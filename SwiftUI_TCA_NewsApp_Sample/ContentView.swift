a6acb412c7ab9b7385cfa7e43c392499a1ff6a09
katafuchix <katafuchix@gmail.com>
2026-04-29 02:36:35 +0900

    Initial Commit

---
+++ SwiftUI_TCA_NewsApp_Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_TCA_NewsApp_Sample
+//
+//  Created by cano on 2026/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_TCA_NewsApp_Sample/SwiftUI_TCA_NewsApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_NewsApp_SampleApp.swift
+//  SwiftUI_TCA_NewsApp_Sample
+//
+//  Created by cano on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_NewsApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
